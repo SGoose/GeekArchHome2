5320943175d7db1c76ba6327e3122de4b470463a
SGoose <dragonandnaruto@gmail.com>
2022-10-25 18:30:32 +0300

    7

--- geek-http-server/src/main/java/ru/geekbrains/HttpServer.java
+++ geek-http-server/src/main/java/ru/geekbrains/HttpServer.java
@@ -1,16 +1,27 @@
 package ru.geekbrains;
 
+import ru.geekbrains.domain.Mapper;
 import ru.geekbrains.logger.ConsoleLogger;
 import ru.geekbrains.logger.Logger;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class HttpServer {
     private static final Logger logger = new ConsoleLogger();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Mapper mapper = new Mapper();
+        Scanner scanner = new Scanner(System.in);
+        String[] scn = scanner.nextLine().split(" ");
+        if (mapper.checkInfo(scn[0], scn[1])){
+            serverStart();
+        }
+    }
+    public static void serverStart(){
         try (ServerSocket serverSocket = new ServerSocket(8088)) {
             logger.info("Server started!");
 
